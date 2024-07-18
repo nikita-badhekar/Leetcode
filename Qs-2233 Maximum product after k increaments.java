2ef8b5fb3c2167346c97a40e300fbcf9e08218db
nikita-badhekar <100899756+nikita-badhekar@users.noreply.github.com>
2024-07-18 12:16:56 +0530

    Update Qs-2233 Maximum product after k increaments.java

--- Qs-2233 Maximum product after k increaments.java
+++ Qs-2233 Maximum product after k increaments.java
@@ -1,6 +1,16 @@
 /*
 2233 - Maximum product after k increaments
 Approach
+
+Increamenting 1 in a smallest element of array. for example 
+nums[] = [0,4]
+if we increamenting in smallest element then only we can get maximum product in 0 and 4 . 
+0 is smallest increament by 4 so nums= [1,4]. 
+1 is smallest increament by 4 so nums= [2,4].
+2 is smallest increament by 4 so nums= [3,4].
+3 is smallest increament by 4 so nums= [4,4].
+4 is equal to 4 so increament any 4 nums= [5,4]. product = 20
+
 Step 1 :Using Priority Queue going to use this approach. As the property of priority queue is to get smallest element everytime when we pop.
 Step 2 : Adding elements to priority queue.
 Step 3 : Increament top element by 1 by using poll() method removes the element at the front end of the container. It deletes the element in the container. then add modified top.
